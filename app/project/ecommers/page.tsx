3b6be6ff55fef0d583a1b1029540f4356a64a450
Vijayabaskar <vj2k02@gmail.com>
2025-02-22 00:10:51 +0530

    feat: update hero image to webp

--- app/project/ecommers/page.tsx
+++ app/project/ecommers/page.tsx
@@ -12,100 +12,100 @@ import { TestimonialSection } from '@/src/sections/testimonial/v1';
 import { basicMetadata } from '@/src/utils/metadata';
 import { getStaggeredDelay } from '@/src/utils/set-staggered-delay';
 import {
- faqSectionData,
- latestUpdate,
- projectDetails,
- servicesData,
- solutions,
- whyChooseSectionData,
+  faqSectionData,
+  latestUpdate,
+  projectDetails,
+  servicesData,
+  solutions,
+  whyChooseSectionData,
 } from './-data.tsx';
 
 export const metadata = basicMetadata({
- title: 'TechAres | Ecommerce App Development Company',
- description:
-  'Custom ecommerce solutions with cutting-edge technology for your business growth',
+  title: 'TechAres | Ecommerce App Development Company',
+  description:
+    'Custom ecommerce solutions with cutting-edge technology for your business growth',
 });
 
 export default function Page() {
- return (
-  <>
-   <HeroSection
-    backgroundImage={'/assets/images/hero/hero-1.webp'}
-    description="Leverage the latest technologies and platforms like Shopify and WooCommerce to ensure your ecommerce solution is robust, scalable, and user-friendly."
-    title="Ecommerce App Development Company"
-    button={{
-     href: '',
-     label: 'Get Quote',
-     openNewTab: true,
-    }}
-   />
-   <ProjectSection projectDetails={projectDetails} />
-   {/* Solution We offer */}
-   <section className="px-4 py-20">
-    <Container>
-     <div className="container mx-auto ">
-      <h2 className="text-center text-xl font-bold text-white md:text-2xl">
-       {solutions?.title}{' '}
-       {solutions?.highlightedTitle && (
-        <span className="text-primary">
-         {solutions?.highlightedTitle}
-        </span>
-       )}
-      </h2>
-      <p className="m mb-8 text-center">{solutions.description}</p>
-      <div className="mx-auto flex max-w-7xl flex-col gap-8 lg:flex-row">
-       {solutions && solutions.solutionCard.length > 0 && (
-        <div className="-mx-4 flex flex-wrap justify-center gap-y-30px">
-         {solutions.solutionCard.map((solution, index) => (
-          <div
-           key={index}
-           data-aos="fade-up"
-           data-aos-delay={getStaggeredDelay([200, 400, 600], index)}
-           className="w-full px-4 md:w-1/2 md:px-[15px]"
-          >
-           <ServiceCard {...solution} />
+  return (
+    <>
+      <HeroSection
+        backgroundImage={'/assets/images/hero/hero-1.webp'}
+        description="Leverage the latest technologies and platforms like Shopify and WooCommerce to ensure your ecommerce solution is robust, scalable, and user-friendly."
+        title="Ecommerce App Development Company"
+        button={{
+          href: '',
+          label: 'Get Quote',
+          openNewTab: true,
+        }}
+      />
+      <ProjectSection projectDetails={projectDetails} />
+      {/* Solution We offer */}
+      <section className="px-4 py-20">
+        <Container>
+          <div className="container mx-auto ">
+            <h2 className="text-center text-xl font-bold text-white md:text-2xl">
+              {solutions?.title}{' '}
+              {solutions?.highlightedTitle && (
+                <span className="text-primary">
+                  {solutions?.highlightedTitle}
+                </span>
+              )}
+            </h2>
+            <p className="m mb-8 text-center">{solutions.description}</p>
+            <div className="mx-auto flex max-w-7xl flex-col gap-8 lg:flex-row">
+              {solutions && solutions.solutionCard.length > 0 && (
+                <div className="-mx-4 flex flex-wrap justify-center gap-y-30px">
+                  {solutions.solutionCard.map((solution, index) => (
+                    <div
+                      key={index}
+                      data-aos="fade-up"
+                      data-aos-delay={getStaggeredDelay([200, 400, 600], index)}
+                      className="w-full px-4 md:w-1/2 md:px-[15px]"
+                    >
+                      <ServiceCard {...solution} />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
-         ))}
-        </div>
-       )}
-      </div>
-     </div>
-    </Container>
-   </section>
-   <ServicesSection
-    servicesData={servicesData}
-    title={{
-     title: 'Features -',
-     highlightedTitle: ' Core Features of Our Ecommerce Platform',
-    }}
-   />
-   {/* Latest Updates  */}
-   <section>
-    <Container>
-     <div className="my-12 text-center  text-white">
-      <h2 className="text-center text-xl font-bold text-white md:text-2xl">
-       <span className="text-primary">{'Advanced Features '}</span>{' '}
-       {'of Our Ecommerce Platform'}
-      </h2>
-      <p>
-       {
-        'Our Advanced Mobile E-Commerce App Development Features To 10X Your Business Growth'
-       }
-      </p>
-     </div>
-     <ServiceSection className="!pt-0" {...latestUpdate} />
-    </Container>
-   </section>
-   <TestimonialSection />
-   <MethodologiesSection
-    methodologies={methodologies}
-    heading={{
-     title: 'Our Development',
-     highlightedTitle: 'Methodologies',
-    }}
-   />
-   <AboutSection aboutSectionData={whyChooseSectionData} />
-   <FAQSection faqSectionData={faqSectionData} />
-  </>
- );
+        </Container>
+      </section>
+      <ServicesSection
+        servicesData={servicesData}
+        title={{
+          title: 'Features -',
+          highlightedTitle: ' Core Features of Our Ecommerce Platform',
+        }}
+      />
+      {/* Latest Updates  */}
+      <section>
+        <Container>
+          <div className="my-12 text-center  text-white">
+            <h2 className="text-center text-xl font-bold text-white md:text-2xl">
+              <span className="text-primary">{'Advanced Features '}</span>{' '}
+              {'of Our Ecommerce Platform'}
+            </h2>
+            <p>
+              {
+                'Our Advanced Mobile E-Commerce App Development Features To 10X Your Business Growth'
+              }
+            </p>
+          </div>
+          <ServiceSection className="!pt-0" {...latestUpdate} />
+        </Container>
+      </section>
+      <TestimonialSection />
+      <MethodologiesSection
+        methodologies={methodologies}
+        heading={{
+          title: 'Our Development',
+          highlightedTitle: 'Methodologies',
+        }}
+      />
+      <AboutSection aboutSectionData={whyChooseSectionData} />
+      <FAQSection faqSectionData={faqSectionData} />
+    </>
+  );
 }
--- data/hero/v1/index.ts
+++ data/hero/v1/index.ts
@@ -1,61 +1,61 @@
 import { HeroProps } from '@/src/sections/hero/v1';
 
 export const heroData: HeroProps = {
- items: [
-  {
-   image: {
-    src: '/assets/images/hero/hero-1.webp',
-    alt: 'hero 1',
-   },
-   title: 'On-demand Development Solutions',
-   button: {
-    label: 'Explore',
-    href: '/',
-   },
-  },
-  {
-   image: {
-    src: '/assets/images/hero/hero-2.webp',
-    alt: 'hero 1',
-   },
-   title: 'Custom Software development',
-   button: {
-    label: 'Explore',
-    href: '/',
-   },
-  },
-  {
-   image: {
-    src: '/assets/images/hero/hero-3.webp',
-    alt: 'hero 1',
-   },
-   title: 'IT consultancy & Development Services',
-   button: {
-    label: 'Explore',
-    href: '/',
-   },
-  },
-  {
-   image: {
-    src: '/assets/images/hero/hero-4.webp',
-    alt: 'hero 1',
-   },
-   title: 'Blockchain Solutions',
-   button: {
-    label: 'Explore',
-    href: '/',
-   },
-  },
-  {
-   image: {
-    src: '/assets/images/hero/hero-5.webp',
-    alt: 'hero 1',
-   },
-   title: 'Hire Developers',
-   button: {
-    label: 'Explore',
-    href: '/',
-   },
-  },
- ],
+  items: [
+    {
+      image: {
+        src: '/assets/images/hero/hero-1.webp',
+        alt: 'hero 1',
+      },
+      title: 'On-demand Development Solutions',
+      button: {
+        label: 'Explore',
+        href: '/',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/hero/hero-2.webp',
+        alt: 'hero 1',
+      },
+      title: 'Custom Software development',
+      button: {
+        label: 'Explore',
+        href: '/',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/hero/hero-3.webp',
+        alt: 'hero 1',
+      },
+      title: 'IT consultancy & Development Services',
+      button: {
+        label: 'Explore',
+        href: '/',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/hero/hero-4.webp',
+        alt: 'hero 1',
+      },
+      title: 'Blockchain Solutions',
+      button: {
+        label: 'Explore',
+        href: '/',
+      },
+    },
+    {
+      image: {
+        src: '/assets/images/hero/hero-5.webp',
+        alt: 'hero 1',
+      },
+      title: 'Hire Developers',
+      button: {
+        label: 'Explore',
+        href: '/',
+      },
+    },
+  ],
 };
