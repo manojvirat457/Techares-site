171d5bc8a9a722457eeaa34e75a041b69b8569e1
Vijayabaskar <vj2k02@gmail.com>
2025-03-19 23:02:47 +0530

    chore: linging fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,49 +11,49 @@ import { Toaster } from 'sonner';
 import './globals.css';
 
 interface Props {
- children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const metadata = basicMetadata({
- title: 'Techares',
- description: 'Techares - IT Solutions and Services',
+  title: 'Techares',
+  description: 'Techares - IT Solutions and Services',
 });
 
 export default async function RootLayout({ children }: Props) {
- return (
-  <html
-   lang="en"
-   suppressHydrationWarning
-   className={cn(
-    'dark',
-    primary.variable,
-    secondary.variable,
-    'text-base leading-[1.875]  text-accent-800 [&.dark]:text-body'
-   )}
-  >
-   <head></head>
-   <AOSInit />
-   <body>
-    <GoogleAnalytics gaId="GTM-W9JNHJXF" />
-    <ThemeProvider attribute="class" defaultTheme="dark">
-     <div
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
       className={cn(
-       'bg-white text-accent-800 dark:bg-accent-900 dark:text-body'
+        'dark',
+        primary.variable,
+        secondary.variable,
+        'text-base leading-[1.875]  text-accent-800 [&.dark]:text-body'
       )}
-     >
-      <MainHeader />
-      <main>{children}</main>
-      <Footer />
-     </div>
-    </ThemeProvider>
-    <Toaster
-     richColors
-     position="top-right"
-     closeButton
-     visibleToasts={9}
-    />
-    <ScrollToTopButton />
-   </body>
-  </html>
- );
+    >
+      <head></head>
+      <AOSInit />
+      <body>
+        <GoogleAnalytics gaId="GTM-W9JNHJXF" />
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <div
+            className={cn(
+              'bg-white text-accent-800 dark:bg-accent-900 dark:text-body'
+            )}
+          >
+            <MainHeader />
+            <main>{children}</main>
+            <Footer />
+          </div>
+        </ThemeProvider>
+        <Toaster
+          richColors
+          position="top-right"
+          closeButton
+          visibleToasts={9}
+        />
+        <ScrollToTopButton />
+      </body>
+    </html>
+  );
 }
