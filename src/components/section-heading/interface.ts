84ef56561020e3508a528fddf7d187953d9feab4
Vijayabaskar <vj2k02@gmail.com>
2025-05-25 12:02:20 +0530

    chore: formatting

--- src/components/section-heading/interface.ts
+++ src/components/section-heading/interface.ts
@@ -4,19 +4,19 @@ type Alignment = 'start' | 'center' | 'end';
 export type TitleSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xm';
 
 export interface SectionHeadingProps {
- subtitle?: string;
- highlighttitle?: string;
- title: string | JSX.Element;
- description?: string;
- alignment?: Alignment;
- textPosition?: Alignment;
- hasBottomSpacing?: boolean;
- className?: ClassValue;
- additionalDesc?: string;
- size?: TitleSize;
+  subtitle?: string;
+  highlighttitle?: string;
+  title: string | JSX.Element;
+  description?: string;
+  alignment?: Alignment;
+  textPosition?: Alignment;
+  hasBottomSpacing?: boolean;
+  className?: ClassValue;
+  additionalDesc?: string;
+  size?: TitleSize;
 }
 
 export type SectionHeadingWithoutStylingProps = Omit<
- SectionHeadingProps,
- 'alignment' | 'hasBottomSpacing'
+  SectionHeadingProps,
+  'alignment' | 'hasBottomSpacing'
 >;
